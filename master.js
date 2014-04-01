a3853ae0e5fccb3ac6fd35bac05b7cc58d5871c8
Tomas Brambora <tomas.brambora@gmail.com>
2014-04-02 08:24:17 +1100

    Salsita version.

--- master.js
+++ master.js
@@ -20,3 +20,28 @@ $jq(window).resize(function() {
 $jq(document).scroll(function() {
   stickyFoot();
 });
+
+// This fixes the bug where multijob plugin wrapper task console icon points
+// to an invalid URL.
+$jq(document).ready(function() {
+  var $links = $jq('a[href^="/view/"]');
+  $links.each(function() {
+    if ($jq(this).attr('href').match(/view\/.+\/view\/.*/)) {
+      $jq(this).attr('href', $jq(this).attr('href').replace(/^\/view\/[^\/]+\/(.+)/, '/$1'));
+    }
+  });
+});
+
+// Add console icons with hyperlinks to the build rows (so that it's easier to see the logs).
+$jq(document).ready(function() {
+  var $links = $jq('.build-row a.model-link');
+  $links.each(function() {
+    $jq(this).after('<a style="margin-left: 10px;" href="' + $jq(this).attr('href') + 'console">' +
+      '<img src="/static/8ce57dc1/images/24x24/terminal.gif"/></a>');
+  });
+});
+
+$jq(document).ready(function() {
+  $jq('a[href="/view/Nested%20View/asynchPeople/"]:eq(1)').text('Salsitistas');
+  $jq('#description div[align="right"] a').text("add a spicy description");
+});
